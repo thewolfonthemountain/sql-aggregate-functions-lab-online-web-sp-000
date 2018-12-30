8eae1aec6458f91976433eaa829a26747f29a316
Sean Langton <slangton1@gmail.com>
2018-12-30 04:12:06 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,11 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "select average(gpa) from students"
+  "select avg(gpa) from students"
 end
 
 def total_tardies_for_all_students
+  "select sum(tardies) from students"
 end
 
 def average_gpa_for_9th_grade
+  "select avg(gpa) from students where grade = 9"
 end
